df71213fc3bf01ec4e88bf088c028da96f87f632
Promise12345678 <promisesmutenhaunga@gmail.com>
2023-03-17 08:19:58 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lo;
+
+	for (lo = 'a'; lo <= 'z'; lo++)
+		putchar(low);
+	for (lo = 'A'; lo <= 'Z'; lo++)
+		putchar(lo);
+		putchar('\n');
+
+	return (0);
+}
