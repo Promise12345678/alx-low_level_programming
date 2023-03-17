58e2758e243161b888c1b126f113a93b35d886bb
Promise12345678 <promisesmutenhaunga@gmail.com>
2023-03-17 09:22:01 -0700

    Print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int pi;
+
+	for (pi = 0; pi < 10; pi++)
+		printf("%d", pi);
+		printf("\n");
+
+	return (0);
+}
