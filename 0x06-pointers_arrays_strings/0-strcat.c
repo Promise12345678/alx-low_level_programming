7668a84f5340830972cfdb4febb81f6d9490c771
Promise12345678 <promisesmutenhaunga@gmail.com>
2023-03-30 15:53:19 -0700

    All changes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - a function that concatenates two strings
+ * _strcat - function concatenates two strings
  * @dest: copy to
  * @src: copy from
  * Return: two concat strings
