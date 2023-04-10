07ebe8d2640c45617c403297e42cde9a23be7774
Promise12345678 <promisesmutenhaunga@gmail.com>
2023-04-10 14:21:26 -0700

    Attack 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <ctype.h>
+
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
+ * main - program that adds positive numbers
+ *
+ * @argc: size of argv array and number of command line arguments
+ *
+ * @argv: An array of size argc
+ *
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j;
+	int sum = 0;
 
 	if (argc < 1)
-		return (0);
+	{
+		printf("0\n");
+
+	}
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
 		sum += atoi(argv[i]);
+
+		for (j = 0; argv[i][j] != '\0'; j++) /*argv is a 2-d array*/
+		{
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+
+				return (0);
+			}
+		}
 	}
+
 	printf("%d\n", sum);
 
 	return (0);
